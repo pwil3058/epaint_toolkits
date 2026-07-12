215168b0e4f6a157f501a7377f7789f491c94830
Peter Williams <pwil3058@bigpond.net.au>
2026-07-13 06:41:05 +1000

    Test PaintDisplay in test gui

--- epaint_gtk_gui/src/main.rs
+++ epaint_gtk_gui/src/main.rs
@@ -1,21 +1,26 @@
 // Copyright (c) 2026 Peter Williams <pwil3058@bigpond.net.au> <pwil3058@gmail.com>.
 
+use std::rc::Rc;
+
 use pw_gtk_ext::{
     gtk::{self, BoxExt, ContainerExt, WidgetExt},
     recollections,
     wrapper::*,
 };
 
-use colour_math::ScalarAttribute::Warmth;
+use colour_math::ScalarAttribute;
+use colour_math::ScalarAttribute::*;
 use colour_math::{HCV, HueConstants};
 
-use epaint::paint::SerializablePaintData;
+use epaint::SeriesId;
+use epaint::paint::{Paint, SerializablePaintData};
 use epaint::properties::{
     Properties,
     PropertyType::{Granulation, Lightfastness, Luminescence, Staining, Transparency},
     PropertyTypes,
 };
 
+use epaint_gtk::series::display::*;
 use epaint_gtk::spec_edit::BasicPaintSpecEditor;
 
 fn main() {
@@ -46,6 +51,23 @@ fn main() {
     paint_spec.name = "name".to_string();
     paint_spec.notes = "notes".to_string();
     bpe.edit(&paint_spec);
+    let paint = Paint::from((
+        paint_spec,
+        Rc::new(SeriesId {
+            series_name: "Series".to_string(),
+            proprietor: "Owner".to_string(),
+        }),
+    ));
+    let mut builder = PaintDisplayBuilder::new();
+    builder
+        .attributes(&[
+            ScalarAttribute::Value,
+            ScalarAttribute::Greyness,
+            ScalarAttribute::Chroma,
+        ])
+        .property_types(&property_types);
+    let display = builder.build(&Rc::new(paint));
+    vbox.pack_start(display.pwo(), true, true, 0);
     vbox.show_all();
     win.add(&vbox);
     win.connect_destroy(|_| gtk::main_quit());
