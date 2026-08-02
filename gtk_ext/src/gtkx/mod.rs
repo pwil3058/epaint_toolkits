df93d7d9da0775c7073e2d8575720ed355a89f35
Peter Williams <pwil3058@bigpond.net.au>
2026-08-02 10:45:57 +1000

    Copy modules from pw_get_ext

---
+++ gtk_ext/src/gtkx/mod.rs
@@ -0,0 +1,7 @@
+// Copyright (c) 2026 Peter Williams <pwil3058@bigpond.net.au> <pwil3058@gmail.com>.
+
+pub mod dialog_user;
+pub mod entry;
+pub mod list_store;
+pub mod tree_model;
+pub mod value;
--- gtk_ext/src/lib.rs
+++ gtk_ext/src/lib.rs
@@ -1,3 +1,16 @@
 // Copyright (c) 2026 Peter Williams <pwil3058@bigpond.net.au> <pwil3058@gmail.com>.
 
+pub use gtk;
+pub use gtk::gdk;
+pub use gtk::gdk_pixbuf;
+pub use gtk::gio;
+pub use gtk::glib;
+
+pub use sourceview;
+
+pub static UNEXPECTED: &str = "Unexpected error: please inform <pwil3058@bigpond.net.au>";
+
+pub mod gtkx;
+
+pub mod printer;
 pub mod wrapper;
